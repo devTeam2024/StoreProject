95bc3327874b7723a4edcecdfd500cac60677ee3
İbrahim Halil Bozay <ibrahimbozay@gmail.com>
2023-06-22 15:25:20 +0300

    ibrahim

---
+++ src/main/java/halilIbrahim/Ibrahim.java
@@ -0,0 +1,9 @@
+package halilIbrahim;
+
+public class Ibrahim {
+    public static void main(String[] args) {
+
+        System.out.println("Push kontrolu");
+        System.out.println("ibrahim");
+    }
+}
